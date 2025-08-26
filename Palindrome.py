5b6d8a43ae3f4ec041e78a8262654cae4dae0d0f
Ruchit Soni <ruchitsoni1898@gmail.com>
2025-08-26 11:27:32 +0200

    check the string is palindrome or not

---
+++ Palindrome.py
@@ -0,0 +1,17 @@
+def is_palindrome(s):
+    i = 0
+    j = len(s) - 1
+
+    # i no longer j i >=j
+    #s['a'] != s['k']
+
+    while i < j:
+        if s[i] != s[j]:
+            return False
+
+        i += 1
+        j -= 1
+    return True
+
+
+print(is_palindrome("aba"))
