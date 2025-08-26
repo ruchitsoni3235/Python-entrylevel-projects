f771c422c0113377da41042825f498695570f13b
Ruchit Soni <ruchitsoni1898@gmail.com>
2025-08-26 11:26:59 +0200

    Non-repeating character project

---
+++ Non-repeating character.py
@@ -0,0 +1,16 @@
+def first_non_repeating_character(s):
+    char_count = {}
+
+    for char in s:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    for i in range(len(s)):
+        if char_count[s[i]] == 1:
+            return i
+
+    return -1
+
+
+print(first_non_repeating_character('Ruchit'))
